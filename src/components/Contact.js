8a448f2624604b16dc71a2da9ef721685d933b27
Dellucifer <pg99285@gmail.com>
2022-09-13 01:33:15 +0530

    Added forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      Contact Us
-    </div>
-  )
-}
+    <div className="container">
+      <div
+        className="center"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "40vh",
+          margin: "auto",
+        }}
+      >
+        <form action="" align="center">
+          <div className="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Name
+            </label>
+            <input
+              className="form-control me-2"
+              type="text"
+              placeholder="Name"
+              aria-label="name"
+            />
+          </div>
 
-export default Contact
+          <div className="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Email address
+            </label>
+            <input
+              className="form-control me-2"
+              type="email"
+              placeholder="Name"
+              aria-label="name"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Message
+            </label>
+            
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
